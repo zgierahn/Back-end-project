9dbffcb3f1b44254d5e96b36a2eda6c905ab497a
Zach Gierahn <zkg2789@yahoo.com>
2023-06-11 15:12:06 -0700

    added-models-migrations-associations

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -5,6 +5,9 @@ module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
       // define association here
+      User.hasMany(models.Review, {foreignKey: 'userId'});
+      User.hasMany(models.Booking, {foreignKey: 'userId'});
+      User.hasMany(models.Spot, {foreignKey: 'ownerId'});
     }
   };
 
