cd7f8562af418adaa208a2eafb90d52569c81cce
Zach Gierahn <zkg2789@yahoo.com>
2023-06-12 13:21:47 -0700

    migrations-options-added

--- backend/db/migrations/20230611212032-create-spot-image.js
+++ backend/db/migrations/20230611212032-create-spot-image.js
@@ -1,5 +1,9 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('SpotImages', {
@@ -28,9 +32,10 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
+    options.tableName = 'SpotImages';
     await queryInterface.dropTable('SpotImages');
   }
 };
