d84e1ccb34c8dff8bbc8a28a444c4b797588b80b
Zach Gierahn <zkg2789@yahoo.com>
2023-06-13 14:29:57 -0700

    post-spots-complete-w-errors-handled

--- backend/routes/api/spot.js
+++ backend/routes/api/spot.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { Spot, SpotImage, Review, User } = require('../../db/models');
 const router = express.Router();
-const { Op } = require('sequelize');
+const { Op, Error } = require('sequelize');
 
 
 router.get('/', async (req, res) => {
@@ -27,6 +27,36 @@ router.get('/', async (req, res) => {
 
     res.json({Spots:newarray});
 });
+
+router.post('/', async (req, res, next) => {
+  let error = {};
+  const {address, city, state, country, lat, lng, name, description, price} = req.body
+    const newSpot = await Spot.build({
+      ownerId: req.user.dataValues.id ? req.user.dataValues.id : 'Must be logged in to create new place',
+      address: address ? address : error.address='Street is required',
+      city: city ? city : error.city = 'City is required',
+      state: state ? state : error.state = 'State is required',
+      country: country ? country : error.country = 'Country is required',
+      lat: lat ? lat : error.lat = 'Latitude is not valid',
+      lng: lng ? lng : error.lng = 'Longitude is not valid',
+      name: name ? name : error.name = 'Name must be less than 50 characters',
+      if(name) {
+        if(name.length > 50) error.name = 'Name must be less than 50 characters'
+      },
+      description: description ? description : error.description = 'Description is required',
+      price: price ? price : error.price = 'Price per day is required'
+    });
+
+if(Object.keys(error).length) {
+  const err = new Error('Bad Resquest');
+  err.error = error;
+  err.statusCode = 400;
+  return next(err);
+}
+   await newSpot.save();
+  res.json(newSpot);
+});
+
 
 
 router.get('/current', async (req, res) => {
@@ -79,23 +109,20 @@ res.json(spotObj);
 
 
 
-
 // router.use((req, res, next) => {
 //     const err = new Error('sorry brah nothing to see here');
 //     err.statusCode = 404;
 //     next(err);
 //   });
 
-//   router.use((err, req, res, next) => {
-//     console.log(err);
-//     const statusCode = err.statusCode || 500;
-//     res.status(statusCode);
-//     res.json({
-//       message: err.message || "these are not the drones you are looking for",
-//       stack: err.stack || 'no stack',
-//       statusCode: statusCode
-//     })
-//   });
+  router.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+      message: err.message || "Bad Request",
+      error: err.error
+    })
+  });
 
 
 module.exports = router;
