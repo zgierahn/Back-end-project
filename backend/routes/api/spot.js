cb8fd83982c23393555b093d6f3a144d9c51a10d
Zach Gierahn <zkg2789@yahoo.com>
2023-06-13 17:32:18 -0700

    delete-spotById-complete

--- backend/routes/api/spot.js
+++ backend/routes/api/spot.js
@@ -109,6 +109,15 @@ router.get('/:spotId', async (req, res) => {
 res.json(spotObj);
 });
 
+router.delete('/:spotId', requireAuth, async (req, res) => {
+  let spot = await Spot.findByPk(req.params.spotId);
+  if(!spot || spot.ownerId !== req.user.dataValues.id) {
+    res.statusCode = 404;
+    return res.json({message: "Spot couldn't be found"});
+  }
+  await spot.destroy();
+  return res.json({"message": "Successfully deleted"})
+});
 
 router.put('/:spotId', requireAuth, async (req, res, next) => {
   let spot = await Spot.findByPk(req.params.spotId);
