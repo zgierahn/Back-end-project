9d5dd33526bcab532ef658fcb8f0bb401ad226d9
Zach Gierahn <zkg2789@yahoo.com>
2023-06-13 17:22:34 -0700

    edit-spotbyId-complete

--- backend/routes/api/spot.js
+++ backend/routes/api/spot.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const { Op, Error } = require('sequelize');
 const {requireAuth} = require('../../utils/auth.js')
 
+
 router.get('/', async (req, res) => {
     let allSpots = await Spot.findAll(
       {include: [{model:SpotImage,
@@ -27,6 +28,7 @@ router.get('/', async (req, res) => {
 
     res.json({Spots:newarray});
 });
+
 
 router.post('/', requireAuth, async (req, res, next) => {
   let error = {};
@@ -108,6 +110,41 @@ res.json(spotObj);
 });
 
 
+router.put('/:spotId', requireAuth, async (req, res, next) => {
+  let spot = await Spot.findByPk(req.params.spotId);
+  if(!spot || spot.ownerId !== req.user.dataValues.id) {
+    res.statusCode = 404;
+    return res.json({message: "Spot couldn't be found"});
+  }
+  let error = {};
+  const {address, city, state, country, lat, lng, name, description, price} = req.body
+     await spot.set({
+      ownerId: req.user.dataValues.id ? req.user.dataValues.id : 'Must be logged in to create new place',
+      address: address ? address : error.address='Street is required',
+      city: city ? city : error.city = 'City is required',
+      state: state ? state : error.state = 'State is required',
+      country: country ? country : error.country = 'Country is required',
+      lat: lat ? lat : error.lat = 'Latitude is not valid',
+      lng: lng ? lng : error.lng = 'Longitude is not valid',
+      name: name ? name : error.name = 'Name must be less than 50 characters',
+      if(name) {
+        if(name.length > 50) error.name = 'Name must be less than 50 characters'
+      },
+      description: description ? description : error.description = 'Description is required',
+      price: price ? price : error.price = 'Price per day is required'
+    });
+
+    if(Object.keys(error).length) {
+      const err = new Error('Bad Resquest');
+      err.error = error;
+      err.statusCode = 400;
+      return next(err);
+    }
+       await spot.save();
+      res.json(spot);
+});
+
+
 router.post('/:spotId/images', requireAuth, async (req, res) => {
   const {url, preview} = req.body;
   const spot = await Spot.findByPk(req.params.spotId);
@@ -120,7 +157,6 @@ router.post('/:spotId/images', requireAuth, async (req, res) => {
         url: 'fresh new image',
         preview: true
   })
-
   res.json({
     id: newImage.id,
     url: newImage.url,
