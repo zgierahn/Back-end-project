86f4504e4c9aaa8623cdebc9613e995a1fbb60b3
Zach Gierahn <zkg2789@yahoo.com>
2023-06-14 18:25:47 -0700

    updated-date-type-model-and-migration

--- backend/db/migrations/20230611212112-create-booking.js
+++ backend/db/migrations/20230611212112-create-booking.js
@@ -20,10 +20,10 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       startDate: {
-        type: Sequelize.DATE
+        type: Sequelize.DATEONLY
       },
       endDate: {
-        type: Sequelize.DATE
+        type: Sequelize.DATEONLY
       },
       createdAt: {
         allowNull: false,
